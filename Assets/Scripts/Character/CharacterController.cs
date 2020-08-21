70132175537f965bfc7bb3b8b3a72f0d82193052
Lugrim <lugrim300@gmail.com>
2020-08-21 19:12:03 +0200

    Added a basic character

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Vector2 movesMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(movesMultiplier * new Vector2(Input.GetAxis("Horizontal"), 0));
+    }
+}
