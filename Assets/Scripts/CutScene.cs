f67d0bd9799d9b6635bb3c203f92cf493bbc86f2
Lugrim <Lugrim@users.noreply.github.com>
2020-08-23 14:00:39 +0200

    Added CutScene

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CutScene : MonoBehaviour
+{
+    public string NextScene = "Hub";
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        VideoPlayer vp = GetComponent<VideoPlayer>();
+        vp.loopPointReached += EndReached;
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(NextScene);
+    }
+}
