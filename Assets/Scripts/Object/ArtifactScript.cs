d73c988efc99a15ff4f83eb4c6effa5d1cb4b072
Alexandre <14941050+doctortee@users.noreply.github.com>
2020-08-23 22:39:58 +0200

    Modified scripts to display an image for 4 second on retrieving an artifact

--- Assets/Scripts/Object/ArtifactScript.cs
+++ Assets/Scripts/Object/ArtifactScript.cs
@@ -7,6 +7,9 @@ public class ArtifactScript : MonoBehaviour
     [Header("Attributes")]
     public int artifactId;
     public string scene;
+    public GameObject fond;
+    public bool transitionning = false;
+
     GameManager meneger = null;
 
     // Start is called before the first frame update
@@ -24,16 +27,24 @@ public class ArtifactScript : MonoBehaviour
 
     // Manages whether the character is on the groud or not
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == "Player") {
-            meneger.GetArtifact(artifactId);
-            LoadNextScene(scene);
+        if (collision.tag == "Player" && !transitionning) {
+            transitionning = true;
+            StartCoroutine(changeScene());
+            transitionning = false;
+        }
     }
-        }/* else {
-            Debug.Log("Colliding");
-            Debug.Log(collision.tag);
-        }*/
 
-     public void LoadNextScene(string scene) {
+    IEnumerator changeScene()
+    {
+        meneger.GetArtifact(artifactId);
+        if (fond != null) {
+            fond.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(4);
         UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
-     }
+        if (fond != null) {
+            fond.SetActive(false);
+        }
+    }
 }
