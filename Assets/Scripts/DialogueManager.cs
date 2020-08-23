a2dd0a2723d6022fced656901b95ab658170c421
Lugrim <Lugrim@users.noreply.github.com>
2020-08-23 15:15:46 +0200

    Added characters images in dialog

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,6 +12,10 @@ public class DialogueManager : MonoBehaviour
     public TextAsset jsonFile;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI lineText;
+    public Image leftImage;
+    public Image rightImage;
+
+    public string basePath = "Assets/Sprites/Dialogues/";
 
     private Dialogue Dial;
 
@@ -25,6 +31,22 @@ public class DialogueManager : MonoBehaviour
     {
         jsonFile = dialogue;
         Dial = JsonUtility.FromJson<Dialogue>(jsonFile.text);
+
+        if (Dial.leftImage != null)
+        {
+            Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(basePath + Dial.leftImage);
+            leftImage.sprite = sprite;
+            leftImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width / 5);
+            leftImage.preserveAspect = true;
+        }
+        if (Dial.rightImage != null)
+        {
+            Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(basePath + Dial.rightImage);
+            rightImage.sprite = sprite;
+            rightImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width / 5); 
+            rightImage.preserveAspect = true;
+        }
+
         line = -1;
         NextLine();
     }
@@ -67,5 +89,7 @@ public class DialogueLine
 public class Dialogue
 {
     public List<DialogueLine> lines;
+    public string leftImage;
+    public string rightImage;
 }
 
