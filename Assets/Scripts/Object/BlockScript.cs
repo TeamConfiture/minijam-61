702e3347f17e3e155907ccf0eba8d7c98c719ded
Alexandre <14941050+doctortee@users.noreply.github.com>
2020-08-23 16:34:35 +0200

    Added a block which moves if all its members are active. Still no direct interaction to lever...

---
+++ Assets/Scripts/Object/BlockScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    [Header("Attributes")]
+    public bool isActive = false;
+    bool oldActive = false;
+
+    [Header("Member")]
+    public GameObject otherBox;
+    public TorchScript[] myGameObjects;
+
+
+    bool moving = false;
+    Vector3 firstPos;
+    Vector3 secondPos;
+    Vector3 futureMove;
+    float endOfMoveTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPos = otherBox.transform.position;
+        secondPos = transform.position;
+        futureMove = firstPos - secondPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool total = true;
+        foreach (TorchScript elem in myGameObjects) {
+            total &= elem.isActive;
+        }
+        isActive = total;
+        if (!moving) {
+            if (isActive != oldActive) {
+                endOfMoveTime = Time.time + 1;
+                oldActive = isActive;
+                futureMove = secondPos - firstPos;
+                var temp = firstPos;
+                firstPos = secondPos;
+                secondPos = temp;
+                moving = true;
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        if (moving) {
+            transform.position = secondPos + (futureMove*Mathf.Max(0,endOfMoveTime - Time.time));
+
+            if (endOfMoveTime - Time.time <= 0) {
+                moving = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Object/LeverScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LeverScript : Interactable
+{
+    [Header("Attributes")]
+    public bool isActive = true;
+
+    [Header("Related Content")]
+    public TorchScript[] myGameObjects;
+
+    private bool prevState = false;
+
+    private SpriteRenderer sRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshPro tmp = GetComponentInChildren<TextMeshPro>();
+        sRenderer = GetComponent<SpriteRenderer>();
+        //theAnim.SetBool("active", isActive); // This means for each primitve data type there's a setter.
+        if(tmp != null)
+        {
+            tmp.text = string.Format(InteractionText, InteractionCommand);
+        }
+
+        MakeInteractable(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prevState != isActive) {
+            prevState = isActive;
+            sRenderer.flipX = isActive;
+            Debug.Log("State switched !");
+            //theAnim.SetBool("active", isActive); // This means for each primitve data type there's a setter.
+            this.ChangeStateObjects();
+        }
+    }
+
+    public void ChangeStateObjects() {
+        foreach (TorchScript elem in myGameObjects) {
+            elem.SwitchState();
+        }
+    }
+
+    public override void Interact() {
+        Debug.Log("Hep !");
+        isActive = !isActive;
+    }
+
+    /*void OnTriggerStay(Collider other){
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.F)){
+            Debug.Log("Hep !");
+        }
+    }*/
+}
