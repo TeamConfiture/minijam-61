d52cc5522c87e1ca69d0d5003ad92aa26bd53044
Lugrim <Lugrim@users.noreply.github.com>
2020-08-23 18:03:27 +0200

    Added music variation

---
+++ Assets/Scripts/ChangeAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAudio : MonoBehaviour
+{
+    public AudioClip music;
+    public float volume = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource source = GameManager.Instance.GetComponent<AudioSource>();
+        source.clip = music;
+        source.volume = volume;
+    }
+}
