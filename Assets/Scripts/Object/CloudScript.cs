148c9d1023fd90d1ef4fe89a9b3f689da912cb67
Kimi <katran@etud.insa-toulouse.fr>
2020-08-31 14:03:21 +0200

    while reading crocodile dialog, it's not raining anymore

--- Assets/Scripts/Object/CloudScript.cs
+++ Assets/Scripts/Object/CloudScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,7 +13,8 @@ public class CloudScript : MonoBehaviour
     private bool isRaining;
     private GameObject rain;
     private Vector3 mousePosition;
-
+    private int line = -1;
+    private int numberOfLines = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,12 +36,15 @@ public class CloudScript : MonoBehaviour
             transform.position = Vector2.Lerp(transform.position, mousePosition, 0.1f);
         }
 
-        if (Input.GetButton("Fire1"))
+        if (Input.GetMouseButtonDown(0))
+        {
+            line++;
+        }
+
+        isRaining = false;
+        if (Input.GetButton("Fire1") && line >= numberOfLines)
         {
             isRaining = true;
-        } else
-        {
-            isRaining = false;
         }
         rain.SetActive(isRaining);
     }
